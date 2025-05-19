934fa9695746d828048317929790ad4e605a6dbe
NATNAEL GETACHEW ASEFA <natigech35@gmail.com>
2025-05-19 18:25:51 -0400

    full connection

--- backend/app.js
+++ backend/app.js
@@ -12,7 +12,12 @@ const app = express();
 app.use("/uploads", express.static("uploads"));
 
 // Middleware
-app.use(cors());
+app.use(
+  cors({
+    origin: "http://localhost:5501", // Your frontend URL
+    credentials: true,
+  })
+);
 app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({ extended: true }));
 app.use(express.json());
